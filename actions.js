51f60f13b0ac8335131bf0c72a4055afdf9b0180
ido <idoitzhak5@gmail.com>
2022-03-14 12:04:43 +0200

    0314

--- actions.js
+++ actions.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+
 router.get('/greeting',(req,res) => {
     return res.status(200).json({
-        message: 'Hello'
+        message: 'Hello',
     });
 });
 module.exports = router;
--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const app = express();
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
-const actionRoute = requrie('./action');
+const actionRoute = requrie('./actions');
 app.use('/api',actionRoute);
+
 const port=5000;
 app.listen(port, function(){
 console.log(`server is working via port ${port}`);
