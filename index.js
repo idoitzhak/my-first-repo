0968b36a6163681ea6dd0e6070fa89e573a93b42
ido <idoitzhak5@gmail.com>
2022-03-14 11:51:35 +0200

    0314

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Welcome');
+const express = require('express');
+const bodyParser = require('body-parser')
+
+const app = express();
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+const actionRoute = requrie('./action');
+app.use('/api',actionRoute);
+const port=5000;
+app.listen(port, function(){
+console.log(`server is working via port ${port}`);
+});
